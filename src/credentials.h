a98df187426b4a525c8c1e8ee9717e568b064b07
ishaankohli <info@skyacres.ca>
2025-02-18 08:34:45 -0800

    Update credentials.h

--- src/credentials.h
+++ src/credentials.h
@@ -2,7 +2,7 @@
 #define CREDENTIALS_H
 
 //Serial number and delays for system
-String serialNumber = "1234567890"; // Unique serial number for each system
+String serialNumber = "123456789043"; // Unique serial number for each system
 
 // Known Wi-Fi Networks
 struct WiFiCredentials {
@@ -11,17 +11,14 @@ struct WiFiCredentials {
 };
 
 const WiFiCredentials knownWiFi[] = {
-    {"Devil_ 4G", "Rathod@2442"},
     {"gladwin22", "Farmer24"},
-    {"Verizon_B3JPTT", "update-fop6-cab"}
-    
 };
 const int knownWiFiCount = sizeof(knownWiFi) / sizeof(knownWiFi[0]);
 
 // Firebase Credentials
 #define API_KEY "AIzaSyDfp9KFIxgs9Wb0AiJTENejm1GLjS2MCQI"
 #define FIREBASE_PROJECT_ID "skyacres-marketplace"
-#define USER_EMAIL "test@gmail.com"
-#define USER_PASSWORD "test123"
+#define USER_EMAIL "info@skyacres.ca"
+#define USER_PASSWORD "SkyacresBC"
 
 #endif // CREDENTIALS_H
