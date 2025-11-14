bb6cb491faad0d6a5cc0950c5a571ae528e06a2b
Dhruman17 <dhruman@skyacres.ca>
2025-11-14 00:23:57 -0500

    Version changed to 1.59

--- src/credentials.h
+++ src/credentials.h
@@ -2,7 +2,7 @@
 #define CREDENTIALS_H
 
 // Serial number and delays for system
-String serialNumber = "HQK1234522"; // Unique serial number for each system
+String serialNumber = "SME102567234"; // Unique serial number for each system
 
 // Known Wi-Fi Networks
 struct WiFiCredentials
